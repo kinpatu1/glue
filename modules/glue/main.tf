332b488c6f5d31adfa6d549b58a1e8d13a539765
miki <kinpatu116@gmail.com>
2023-09-08 05:43:10 +0900

    add rds

--- modules/glue/main.tf
+++ modules/glue/main.tf
@@ -18,13 +18,13 @@ resource "aws_security_group" "glue" {
   }
 
   ingress {
-    cidr_blocks      = ["0.0.0.0/0"]
-    from_port        = "22"
+    cidr_blocks      = []
+    from_port        = "0"
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
     protocol         = "tcp"
     security_groups  = []
-    self             = false
-    to_port          = "22"
+    self             = true
+    to_port          = "65535"
   }
 }

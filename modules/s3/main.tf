8ee68f92fad369a219ae01a63fc7b2269f5d50e5
きんぱつ <59814055+kinpatu1@users.noreply.github.com>
2023-09-11 05:37:37 +0900

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -13,8 +13,3 @@ resource "aws_s3_bucket" "s3_bucket" {
     Name = var.s3_bucket_name
   }
 }
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket      = aws_s3_bucket.s3_bucket.id
-  eventbridge = true
-}

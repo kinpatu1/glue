cf487ea722d0930f083c8f11ea22053bce7d42b2
miki <kinpatu116@gmail.com>
2023-09-10 23:05:00 +0900

    add s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.s3_bucket_name
 
-            "server_side_encryption_configuration": [
+            server_side_encryption_configuration [
               {
                 "rule": [
                   {

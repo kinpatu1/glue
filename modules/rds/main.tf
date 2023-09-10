1f2d2fc0ec9a7e73980f5233e451b08d887a8197
miki <kinpatu116@gmail.com>
2023-09-10 23:23:59 +0900

    fix rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -8,7 +8,6 @@ resource "aws_rds_cluster" "cluster" {
   deletion_protection  = false
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.public.name
-  apply_immediately = true
   vpc_security_group_ids = [
     aws_security_group.rds.id
   ]
@@ -34,7 +33,6 @@ resource "aws_rds_cluster_instance" "instance" {
   instance_class     = "db.t3.small"
   engine             = "aurora-mysql"
   engine_version     = "5.7.mysql_aurora.2.11.2"
-  apply_immediately = true
 }
 
 resource "aws_security_group" "rds" {

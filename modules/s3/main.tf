2c62ab03cba683f3391bdde97dca0546ecc91d1f
miki <kinpatu116@gmail.com>
2023-09-10 23:03:50 +0900

    add s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,9 +1,21 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.s3_bucket_name
 
-  bucket_key_enabled = false
-  sse_algorithm = "AES256"
-  region  = ap-northeast-1
+            "server_side_encryption_configuration": [
+              {
+                "rule": [
+                  {
+                    "apply_server_side_encryption_by_default": [
+                      {
+                        "kms_master_key_id": "",
+                        "sse_algorithm": "AES256"
+                      }
+                    ],
+                    "bucket_key_enabled": false
+                  }
+                ]
+              }
+            ]
 
   tags = {
     Name = var.s3_bucket_name

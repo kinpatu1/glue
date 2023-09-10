c61595d2981c9c766dbb17a58026677d92136ce9
miki <kinpatu116@gmail.com>
2023-09-11 00:53:59 +0900

    add glue

---
+++ modules/iam/main.tf
@@ -0,0 +1,16 @@
+resource "aws_iam_role" "glue_connection" {
+  name               = var.iam_role
+  assume_role_policy = data.template_file.assume_role_json.rendered
+}
+
+resource "aws_iam_role_policy" "glue_connection" {
+  name = var.iam_policy
+  role = aws_iam_role.glue_connection.id
+
+  policy = arn:aws:iam::aws:policy/service-role/AWSGlueDataBrewServiceRole
+}
+
+resource "aws_iam_role_policy_attachment" "glue_connection" {
+  role       = aws_iam_role.glue_connection.id
+  policy_arn = aws_iam_role_policy.glue_connection.id
+}

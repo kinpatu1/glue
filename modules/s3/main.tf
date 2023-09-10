dd62b04cf807f886e544354b979c15bac13c0154
miki <kinpatu116@gmail.com>
2023-09-10 23:07:34 +0900

    add s3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "s3_bucket" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
+        sse_algorithm = "AES256"
       }
     }
   }

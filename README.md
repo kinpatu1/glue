9861622eb91c03184c724b5563cf184f51cf553f
miki <kinpatu116@gmail.com>
2023-09-11 02:11:34 +0900

    add glue

---
+++ README.md
@@ -0,0 +1,6 @@
+## テーブル作成
+create table acinfotokyo (
+id serial primary key,
+time timestamp,
+voltage double,
+frequency double);
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -7,7 +7,7 @@ resource "aws_rds_cluster" "cluster" {
   engine               = "aurora-mysql"
   engine_version       = "5.7.mysql_aurora.2.11.2"
   availability_zones   = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
-  master_username      = "admin"
+  master_username      = var.master_username
   master_password      = var.master_password
   deletion_protection  = false
   skip_final_snapshot  = true
